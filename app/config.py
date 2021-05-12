7c88895cb1c5c8053685399ee0affde0d633004e
James Lentzsch <jrlentzsch@gmail.com>
2021-05-12 13:05:08 -0700

    SQLAlchemy set up with bug

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Configuration:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL") or \
+        "postgresql://sqlalchemy_test:password@localhost/sqlalchemy_test"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/routes/orders.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+bp = Blueprint('orders', __name__, url_prefix='')
+
+
+@bp.route('/')
+def index():
+    return 'Order Up!'
+
+    
