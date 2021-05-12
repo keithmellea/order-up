3a72c16585375fdfa72dd976faf8fd1dbaae9571
Keith Mellea <keithmellea@gmail.com>
2021-05-12 16:49:54 -0400

    login

--- app/config.py
+++ app/config.py
@@ -1,8 +1,7 @@
 import os
 
-
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL") or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
         "postgresql://sqlalchemy_test:password@localhost/sqlalchemy_test"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
